cdde6953cf121eb6c84fc5f480d60ee6e2e3d47a
Aidan Dennehy <duinneacha@gmail.com>
2019-02-07 16:19:12 +0000

    Initial commit 16:20

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+   {id: 11, name: 'Donny Silvino'},
+   {id: 12, name: 'Matt Carbunkle'},
+   {id: 13, name: 'Ssandra Ellis'},
+   {id: 14, name: 'Fredrick Jaywardee'},
+   {id: 15, name: 'Jonathan Henry'},
+   {id: 16, name: 'Ricky Pomphrett'},
+   {id: 17, name: 'Geraldo Bermuda'},
+   {id: 18, name: 'Angelo Bay'}
+
+]
